ede901eda31896f17417a4232ff0c3ee73d9b1b8
Salju <61861078+Jusey1@users.noreply.github.com>
2024-07-01 16:42:12 -0400

    Add files via upload

--- src/main/java/net/salju/supernatural/init/SupernaturalTags.java
+++ src/main/java/net/salju/supernatural/init/SupernaturalTags.java
@@ -7,7 +7,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.registries.Registries;
 
 public class SupernaturalTags {
-	public static final TagKey KOBOLD = TagKey.create(Registries.ENTITY_TYPE, new ResourceLocation("forge:kobolds"));
+	public static final TagKey KOBOLD = TagKey.create(Registries.ENTITY_TYPE, new ResourceLocation("kobolds:kobolds"));
 	public static final TagKey SPOOKY = TagKey.create(Registries.ENTITY_TYPE, new ResourceLocation("supernatural:spook_targets"));
 	public static final TagKey MERRY = TagKey.create(Registries.ENTITY_TYPE, new ResourceLocation("supernatural:mer_targets"));
 	public static final TagKey VAMPIRE = TagKey.create(Registries.ENTITY_TYPE, new ResourceLocation("supernatural:is_vampire"));
