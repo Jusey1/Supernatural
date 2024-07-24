ec2ba8a7ac40dbe5bd173ebda5daa551c186d3c3
Salju <61861078+Jusey1@users.noreply.github.com>
2024-07-24 01:27:12 -0400

    Delete src/main/java/net/salju/supernatural directory

--- src/main/java/net/salju/supernatural/client/renderer/ArtCanRenderer.java
+++
@@ -1,35 +0,0 @@
-package net.salju.supernatural.client.renderer;
-
-import net.salju.supernatural.entity.Cannon;
-import net.salju.supernatural.client.model.ArtCanModel;
-import net.minecraft.util.Mth;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.client.renderer.entity.LivingEntityRenderer;
-import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import com.mojang.math.Axis;
-import com.mojang.blaze3d.vertex.PoseStack;
-
-public class ArtCanRenderer extends LivingEntityRenderer<Cannon, ArtCanModel<Cannon>> {
-	public ArtCanRenderer(EntityRendererProvider.Context context) {
-		super(context, new ArtCanModel(context.bakeLayer(ArtCanModel.CANNON_MODEL)), 0.85f);
-	}
-
-	@Override
-	public ResourceLocation getTextureLocation(Cannon target) {
-		return new ResourceLocation("supernatural:textures/entity/artificer_cannon_" + Integer.toString(target.getCannonType()) + ".png");
-	}
-
-	@Override
-	protected boolean shouldShowName(Cannon target) {
-		return (!target.isEmpty());
-	}
-
-	@Override
-	protected void setupRotations(Cannon target, PoseStack pose, float f1, float f2, float f3) {
-		pose.mulPose(Axis.YP.rotationDegrees(180.0F - f2));
-		float f = (float) (target.level().getGameTime() - target.lastHit) + f3;
-		if (f < 5.0F) {
-			pose.mulPose(Axis.YP.rotationDegrees(Mth.sin(f / 1.5F * (float) Math.PI) * 3.0F));
-		}
-	}
-}
--- src/main/java/net/salju/supernatural/events/SupernaturalClientEvents.java
+++
@@ -1,24 +0,0 @@
-package net.salju.supernatural.events;
-
-import net.salju.supernatural.init.SupernaturalItems;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.client.event.RenderHandEvent;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.client.Minecraft;
-import com.mojang.math.Axis;
-
-@Mod.EventBusSubscriber(value = Dist.CLIENT)
-public class SupernaturalClientEvents {
-	@SubscribeEvent
-	public static void onRenderHand(RenderHandEvent event) {
-		if (event.getItemStack().is(SupernaturalItems.MAGIC_MIRROR.get()) && Minecraft.getInstance().player.isUsingItem() && event.getItemStack() == Minecraft.getInstance().player.getUseItem()) {
-			float r = (event.getHand() == InteractionHand.MAIN_HAND ? 21.0F : -21.0F);
-			float t = (event.getHand() == InteractionHand.MAIN_HAND ? -0.185F : 0.185F);
-			event.getPoseStack().translate(t, 0.015F, 0.02F);
-			event.getPoseStack().mulPose(Axis.XP.rotationDegrees(-3.0F));
-			event.getPoseStack().mulPose(Axis.ZP.rotationDegrees(r));
-		}
-	}
-}
