1135459490e93fcaebf76c75a6941a215dc5dffc
Salju <61861078+Jusey1@users.noreply.github.com>
2024-10-07 23:14:15 -0400

    Removed Unused Files

--- src/main/java/net/salju/supernatural/network/NetworkSetup.java
+++
@@ -1,14 +0,0 @@
-package net.salju.supernatural.network;
-
-import net.salju.supernatural.SupernaturalMod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-
-@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
-public class NetworkSetup {
-	@SubscribeEvent
-	public static void registerMessage(FMLCommonSetupEvent event) {
-		SupernaturalMod.addNetworkMessage(UsedContract.class, UsedContract::buffer, UsedContract::reader, UsedContract::handler);
-	}
-}
--- src/main/java/net/salju/supernatural/network/UsedContract.java
+++
@@ -1,34 +0,0 @@
-package net.salju.supernatural.network;
-
-import net.salju.supernatural.events.SupernaturalClientManager;
-import net.minecraftforge.network.NetworkEvent;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.network.FriendlyByteBuf;
-import java.util.function.Supplier;
-
-public class UsedContract {
-	private static ItemStack stack;
-
-	public UsedContract(ItemStack item) {
-		this.stack = item;
-	}
-
-	public static UsedContract reader(FriendlyByteBuf buffer) {
-		return new UsedContract(buffer.readItem());
-	}
-
-	public static void buffer(UsedContract message, FriendlyByteBuf buffer) {
-		buffer.writeItem(stack);
-	}
-
-	public static void handler(UsedContract message, Supplier<NetworkEvent.Context> context) {
-		context.get().enqueueWork(() -> {
-			Player player = SupernaturalClientManager.getPlayer(context.get().getDirection().getReceptionSide());
-			if (player != null) {
-				SupernaturalClientManager.usedContract(player, stack);
-			}
-		});
-		context.get().setPacketHandled(true);
-	}
-}
