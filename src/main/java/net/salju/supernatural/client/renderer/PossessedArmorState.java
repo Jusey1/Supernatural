5f3f3d07fe50c4bfd7fca58224ba249dd7a4b80a
Salju <61861078+Jusey1@users.noreply.github.com>
2025-10-28 11:04:07 -0400

    Deleted Unused State

--- src/main/java/net/salju/supernatural/client/renderer/PossessedArmorState.java
+++
@@ -1,8 +0,0 @@
-package net.salju.supernatural.client.renderer;
-
-import net.minecraft.client.renderer.entity.state.HumanoidRenderState;
-
-public class PossessedArmorState extends HumanoidRenderState {
-	public boolean isAggressive;
-	public boolean isLeftHanded;
-}
