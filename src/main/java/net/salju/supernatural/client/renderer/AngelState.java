161c1f7809d11043939ec9cdc1be992869ca4de8
Salju <61861078+Jusey1@users.noreply.github.com>
2025-10-28 11:03:53 -0400

    Deleted Unused State

--- src/main/java/net/salju/supernatural/client/renderer/AngelState.java
+++
@@ -1,7 +0,0 @@
-package net.salju.supernatural.client.renderer;
-
-import net.minecraft.client.renderer.entity.state.HumanoidRenderState;
-
-public class AngelState extends HumanoidRenderState {
-	public int angelPose;
-}
