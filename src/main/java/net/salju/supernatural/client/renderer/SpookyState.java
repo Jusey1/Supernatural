c9b52da3e0dc5b5375be11c5a859996da1bd4b39
Salju <61861078+Jusey1@users.noreply.github.com>
2025-10-28 11:04:19 -0400

    Deleted Unused State

--- src/main/java/net/salju/supernatural/client/renderer/SpookyState.java
+++
@@ -1,7 +0,0 @@
-package net.salju.supernatural.client.renderer;
-
-import net.minecraft.client.renderer.entity.state.HumanoidRenderState;
-
-public class SpookyState extends HumanoidRenderState {
-	public boolean isCastingSpell;
-}
