ef74e5dd5ae1a0c26c982836b5065e8e7842d329
Salju <61861078+Jusey1@users.noreply.github.com>
2026-02-20 11:00:56 -0500

    Add files via upload

--- src/main/java/net/salju/supernatural/events/SupernaturalManager.java
+++ src/main/java/net/salju/supernatural/events/SupernaturalManager.java
@@ -194,7 +194,7 @@ public class SupernaturalManager {
 	}
 
 	public static boolean canSoulMagicWork(ServerLevel lvl, BlockPos pos) {
-		return (lvl.dimension().equals(Level.OVERWORLD) && lvl.getBrightness(LightLayer.BLOCK, pos) <= 3 && (lvl.getBrightness(LightLayer.SKY, pos) <= 3 || lvl.isDarkOutside()));
+		return (lvl.dimension().equals(Level.OVERWORLD) && lvl.getBrightness(LightLayer.BLOCK, pos) <= 4 && (lvl.getBrightness(LightLayer.SKY, pos) <= 4 || lvl.isDarkOutside()));
 	}
 
 	public static int getPower(ServerLevel lvl, BlockPos pos, Player player) {
