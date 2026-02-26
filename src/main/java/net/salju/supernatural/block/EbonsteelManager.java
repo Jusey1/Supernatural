0b549e5125d4e8d1ee48b804964b9ff17656b654
Salju <61861078+Jusey1@users.noreply.github.com>
2026-02-25 20:51:28 -0500

    Add files via upload

--- src/main/java/net/salju/supernatural/block/EbonsteelManager.java
+++ src/main/java/net/salju/supernatural/block/EbonsteelManager.java
@@ -6,6 +6,6 @@ import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.SoundType;
 
 public class EbonsteelManager {
-    public static final BlockSetType EBONSTEEL = BlockSetType.register(new BlockSetType("ebonsteel", true, false, false, BlockSetType.PressurePlateSensitivity.MOBS, SoundType.NETHERITE_BLOCK, SoundEvents.WOODEN_DOOR_CLOSE, SoundEvents.WOODEN_DOOR_OPEN, SoundEvents.WOODEN_TRAPDOOR_CLOSE, SoundEvents.WOODEN_TRAPDOOR_OPEN, SoundEvents.METAL_PRESSURE_PLATE_CLICK_OFF, SoundEvents.METAL_PRESSURE_PLATE_CLICK_ON, SoundEvents.STONE_BUTTON_CLICK_OFF, SoundEvents.STONE_BUTTON_CLICK_ON));
+    public static final BlockSetType EBONSTEEL = BlockSetType.register(new BlockSetType("ebonsteel", true, false, false, BlockSetType.PressurePlateSensitivity.MOBS, SoundType.NETHERITE_BLOCK, SoundEvents.IRON_DOOR_CLOSE, SoundEvents.IRON_DOOR_OPEN, SoundEvents.IRON_TRAPDOOR_CLOSE, SoundEvents.IRON_TRAPDOOR_OPEN, SoundEvents.METAL_PRESSURE_PLATE_CLICK_OFF, SoundEvents.METAL_PRESSURE_PLATE_CLICK_ON, SoundEvents.STONE_BUTTON_CLICK_OFF, SoundEvents.STONE_BUTTON_CLICK_ON));
     public static final BooleanProperty LOCKED = BooleanProperty.create("locked");
 }
--- src/main/java/net/salju/supernatural/block/EbonsteelTrapDoorBlock.java
+++ src/main/java/net/salju/supernatural/block/EbonsteelTrapDoorBlock.java
@@ -1,7 +1,5 @@
 package net.salju.supernatural.block;
 
-import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.gameevent.GameEvent;
 import net.salju.supernatural.init.SupernaturalItems;
 import net.minecraft.core.BlockPos;
 import net.minecraft.sounds.SoundEvents;
@@ -9,20 +7,21 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.DoorBlock;
+import net.minecraft.world.level.block.TrapDoorBlock;
+import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.level.redstone.Orientation;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
-
 import javax.annotation.Nullable;
 
-public class EbonsteelDoorBlock extends DoorBlock {
-	public EbonsteelDoorBlock(BlockBehaviour.Properties props) {
+public class EbonsteelTrapDoorBlock extends TrapDoorBlock {
+	public EbonsteelTrapDoorBlock(BlockBehaviour.Properties props) {
 		super(EbonsteelManager.EBONSTEEL, props);
 	}
 
@@ -37,7 +36,7 @@ public class EbonsteelDoorBlock extends DoorBlock {
         if (state.getValue(EbonsteelManager.LOCKED)) {
             if (stack.is(SupernaturalItems.EBONSTEEL_KEY.get())) {
                 world.setBlock(pos, this.getState(state, false).cycle(OPEN), 10);
-                world.playSound(player, pos, SoundEvents.IRON_DOOR_OPEN, SoundSource.BLOCKS);
+                world.playSound(player, pos, SoundEvents.IRON_TRAPDOOR_OPEN, SoundSource.BLOCKS);
                 world.gameEvent(player, GameEvent.BLOCK_OPEN, pos);
                 if (!player.isCreative()) {
                     stack.shrink(1);
