55779bb12af07b56518c8c0fd003362b76d697fc
Salju <61861078+Jusey1@users.noreply.github.com>
2026-05-30 16:30:21 -0400

    Reset Folder

--- src/main/java/net/salju/supernatural/client/renderer/VampireRenderState.java
+++
@@ -1,7 +0,0 @@
-package net.salju.supernatural.client.renderer;
-
-import net.minecraft.client.renderer.entity.state.IllagerRenderState;
-
-public class VampireRenderState extends IllagerRenderState {
-    public String type;
-}
